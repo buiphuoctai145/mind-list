da6ec3c0d01ef1d9dc298a7cb3d3aa585ea707f2
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2023-06-08 23:15:40 +0700

    modal create new task

---
+++ src/components/MindList.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import CreateTask from "../modals/createTask";
+
+const MindList = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => {
+    setModal(!modal);
+  }
+  return (
+    <div>
+      <div className="header-container text-center">
+        <h3>MindList</h3>
+        <button className="btn btn-primary mt-3" onClick={() => setModal(true)}>
+          Create task
+        </button>
+      </div>
+      <div className="task-container"></div>
+      <CreateTask toggle={toggle} modal={modal} />
+    </div>
+  );
+};
+
+export default MindList;
