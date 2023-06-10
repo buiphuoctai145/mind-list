a0c7ac656e9a1fa3db22aba7e8964bdfd1e5d869
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2023-06-10 12:28:47 +0700

    restyle create task modal

--- src/components/modals/CreateTasks.tsx
+++ src/components/modals/CreateTasks.tsx
@@ -2,25 +2,36 @@ import React from "react";
 
 export const CreateTask = ({ isVisible, onClose }: { isVisible: boolean; onClose: () => void }) => {
   // const [taskName, setTaskname] = useState("");
+  const style = {
+    modal: `fixed top-0 h-screen w-screen bg-black/80 flex flex-col items-center justify-center`,
+    container: `flex flex-col bg-white rounded-lg p-5`,
+    modalheader:`text-2xl font-medium text-center`,
+    tasknamelabel:`block font-bold mb-2`,
+    descriptionlabel:`block font-bold mb-2`,
+    tasknameinput:`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight focus:outline-none focus:shadow-outline`,
+    descriptiontextarea:`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`,
+    createbutton:`bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline`,
+    cancelbutton:`bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline`,
 
+  };
   if (!isVisible) return null;
   return (
-    <div className="fixed top-0 h-screen w-screen bg-black/80 flex flex-col items-center justify-center">
-      <div className="flex flex-col bg-white rounded-lg p-5">
-        <div className="text-2xl font-medium text-center">Create new task</div>
-        <div className="flex">
-          <label>Task name</label>
-          <input type="text" className="border ml-2 rounded-sm border-gray-200" />
+    <div className={style.modal}>
+      <div className={style.container}>
+        <div className={style.modalheader}>Create new task</div>
+        <div className="mb-4">
+          <label className={style.tasknamelabel}>Task name</label>
+          <input type="text" className={style.tasknameinput} />
         </div>
-        <div className="mt-2 form-group">
-          <label>Description</label>
-          <textarea rows={6} className="form-control" />
+        <div className="mt-2 mb-4 form-group">
+          <label className={style.descriptionlabel}>Description</label>
+          <textarea rows={6} className={style.descriptiontextarea} />
         </div>
         <div className="flex items-center gap-2">
-          <button className="bg-blue-300 p-2 text-white" onClick={onClose}>
+          <button className={style.createbutton} onClick={onClose}>
             Create
           </button>
-          <button className="bg-blue-300 p-2 text-white" onClick={onClose}>
+          <button className={style.cancelbutton} onClick={onClose}>
             Cancel
           </button>
         </div>
