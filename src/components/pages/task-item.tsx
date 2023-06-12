c1f7a74aed3898ee57c21ed3853cbc7638250d2b
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2023-06-12 19:13:20 +0700

    restyle task item

--- src/components/pages/task-item.tsx
+++ src/components/pages/task-item.tsx
@@ -11,35 +11,44 @@ export const TaskItem = ({
   onRemove?: () => void;
 }) => {
   return (
-    <div className={`flex shadow p-2 items-center rounded-sm" ${data.checked ? "bg-gray-500" : "bg-green-300"}`}>
-      <div className="flex flex-col flex-1">
-        <div
-          className="w-6 h-6 bg-white rounded-md border border-gray-300 flex flex-col items-center justify-center"
-          onClick={onCheck}
-        >
-          {data.checked ? (
-            <div className="w-4 h-4 bg-gray-500 rounded-md" />
-          ) : null}
+    <div
+      className={`flex shadow p-2 items-center rounded-sm" ${
+        data.checked ? "bg-gray-500" : "bg-green-300"
+      }`}
+    >
+      <div className="flex">
+        <div className="flex-none w-14">
+          <div
+            className="w-6 h-6 bg-white rounded-md border border-gray-300 flex flex-col items-center justify-center"
+            onClick={onCheck}
+          >
+            {data.checked ? (
+              <div className="w-4 h-4 bg-gray-500 rounded-md" />
+            ) : null}
+          </div>
         </div>
-        {data.checked ? (
-          <p className="font-bold line-through">{data.name}</p>
-        ) : (
-          <p className="font-bold">{data.name}</p>
-        )}
-
-        {data.checked ? (
-          <p className="text-sm line-through">{data.description}</p>
-        ) : (
-          <p className="text-sm">{data.description}</p>
-        )}
-       
       </div>
-      <button
-        onClick={onRemove}
-        className="bg-red-500 p-2 rounded-md text-white"
-      >
-        Delete
-      </button>
+      <div className="grow">
+          {data.checked ? (
+            <p className="font-bold line-through">{data.name}</p>
+          ) : (
+            <p className="font-bold">{data.name}</p>
+          )}
+
+          {data.checked ? (
+            <p className="text-sm line-through">{data.description}</p>
+          ) : (
+            <p className="text-sm">{data.description}</p>
+          )}
+        </div>
+      <div className="flex-none">
+          <button
+            onClick={onRemove}
+            className="bg-red-500 p-2 rounded-md text-white"
+          >
+            Delete
+          </button>
+        </div>
     </div>
   );
 };
